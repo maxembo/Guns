f0e32ad9ba54144ec72926ab22ef92bf1c6f19c6
maxembo <91334358+maxembo@users.noreply.github.com>
2023-11-17 21:27:05 +0300

    plant one bullet

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Bullet
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField,Range(0,10)] private float speed = 2;
+
+        private void Start() => Destroy(gameObject, 5f);
+
+        private void Update() => transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        
+        
+    }
+}
---
+++ Assets/Scripts/Plants/PlantOneBullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Plants
+{
+    public class PlantOneBullet : MonoBehaviour
+    {
+        [SerializeField] private Bullet.Bullet bulletPrefab;
+        [SerializeField] private Transform bulletSpawnPoint;
+        [SerializeField, Range(0, 10)] private float reloadTime;
+
+        private float _currentReloadTime;
+
+        public void Shoot()
+        {
+            if (!(_currentReloadTime < 0)) return;
+            
+            Instantiate(bulletPrefab,bulletSpawnPoint.position,Quaternion.identity);
+            _currentReloadTime = reloadTime;
+        }
+
+        private void Update()
+        {
+            if (_currentReloadTime < 0) return;
+
+            _currentReloadTime -= Time.deltaTime;
+        }
+    }
+}
